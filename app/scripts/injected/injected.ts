61352e9ae98b635d08b88d6b1e08ffdbe6a5f164
TinyMan <raph.a.uzan@gmail.com>
2018-04-07 18:45:13 +0200

    Get Cart on first visit

--- app/scripts/injected/injected.ts
+++ app/scripts/injected/injected.ts
@@ -10,30 +10,32 @@ XMLHttpRequest.prototype.open = function (this: XMLHttpRequest, method, url, ...
   if (method === 'GET' && url.match(/strReloadPanierRight\.php/)) {
     this.addEventListener("readystatechange", e => {
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        const regex = /(\d+)&nbsp;x<\/span>([^<]+)<[\s\S]+?>([\d\.]+)&nbsp;€<[\s\S]+?onclick="ModifProdPanier\('(\d+)'/g;
-        const items: CartItem[] = []
-        let match = regex.exec(this.responseText);
-        while (match != null) {
-          const [, qty, title, price, id] = match;
-          items.push({
-            qty: parseInt(qty, 10),
-            title: title.trim(),
-            id,
-            price: parseFloat(price),
-          })
-          match = regex.exec(this.responseText);
-        }
-
-        updateCart({
-          items,
-          total: items.reduce((a, e) => a + e.price, 0),
-        });
+        updateCart(getCart(this.responseText));
       }
     })
   }
   return old.call(this, method, url, ...args);
 }
 
+function getCart(html: string) {
+  const regex = /(\d+)&nbsp;x<\/span>([^<]+)<[\s\S]+?>([\d\.]+)&nbsp;€<[\s\S]+?onclick="ModifProdPanier\('(\d+)'/g;
+  const items: CartItem[] = []
+  let match = regex.exec(html);
+  while (match != null) {
+    const [, qty, title, price, id] = match;
+    items.push({
+      qty: parseInt(qty, 10),
+      title: title.trim(),
+      id,
+      price: parseFloat(price),
+    })
+    match = regex.exec(html);
+  }
+  return {
+    items,
+    total: items.reduce((a, e) => a + e.price, 0),
+  };
+}
 
 function updateCart(cart: Cart) {
   console.log(cart);
@@ -44,4 +46,4 @@ function sendMessage(message: MinaTopMessage, callback?: (response: any) => void
   return chrome.runtime.sendMessage(EXT_ID, message, callback);
 }
 
-// sendMessage(new EchoMessage({ complex: 'message', object: { key: 1 } }), e => console.log(e));
+updateCart(getCart(document.querySelector('#PanierRight')!.innerHTML));
