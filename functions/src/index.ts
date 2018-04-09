c58997c7298d421141d358a62be4adb17c9bf075
TinyMan <raph.a.uzan@gmail.com>
2018-04-09 21:01:41 +0200

    functions: update currentOrder on order creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+export const onCreateOrder = functions.firestore.document('groups/{group}/orders/{order}').onCreate((snap, context) => {
+  const { group, order } = context.params;
+
+  return snap.ref.parent.parent.update({
+    currentOrder: order
+  });
+})
